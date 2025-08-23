13936c950428013cfc8be732f76eefdb07452e90
Aliencryptodev <aliencryptodev@gmail.com>
2025-08-24 01:46:20 +0200

    Update page.tsx

--- app/battle/page.tsx
+++ app/battle/page.tsx
@@ -1,12 +1,9 @@
-// app/battle/page.tsx  (SERVER COMPONENT)
-import NextDynamic from 'next/dynamic';
-
-// Fuerza render dinámico (evita SSG/ISR)
+// app/battle/page.tsx (Server Component)
 export const dynamic = 'force-dynamic';
 export const revalidate = false;
 
-// Cargar el cliente de Phaser sólo en el navegador
-const BattleClient = NextDynamic(() => import('./BattleClient'), { ssr: false });
+import dynamicImport from 'next/dynamic';
+const BattleClient = dynamicImport(() => import('./BattleClient'), { ssr: false });
 
 export default function Page() {
   return <BattleClient />;
