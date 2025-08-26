c0f7ceaefc5e75b4ddce2233d1e31f1d30633c45
Aliencryptodev <aliencryptodev@gmail.com>
2025-08-26 17:26:17 +0200

    Update page.tsx

--- app/battle/page.tsx
+++ app/battle/page.tsx
@@ -1,11 +1,13 @@
-import dynamic from 'next/dynamic';
+// app/battle/page.tsx
+
+import NextDynamic from 'next/dynamic';
 
 // Fuerza render dinámico y sin caché/ISR para este segmento
 export const dynamic = 'force-dynamic';
 export const revalidate = false;
 
 // Renderiza el juego SOLO en cliente (sin SSR)
-const BattleClient = dynamic(() => import('./BattleClient'), { ssr: false });
+const BattleClient = NextDynamic(() => import('./BattleClient'), { ssr: false });
 
 export default function Page() {
   return <BattleClient />;
